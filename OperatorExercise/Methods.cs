1d99f6f56bc1b13b85b8cf317c87e23c1da39a51
Cody <Cojo341@gmail.com>
2022-08-09 15:35:50 -0400

    initial commit

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorExercise
+{
+    internal class Methods
+    {
+        public static int Sum(params int[] addNumbers)
+        {
+            int total1 = 0;
+
+            foreach (int y in addNumbers)
+            {
+                total1 += y;
+            }
+            return total1;
+
+        }
+        public static double Multiply(params int[] mulNumbers)
+
+        {
+            int total2 = 1;
+
+            foreach (int y in mulNumbers)
+            {
+                total2 *= y;
+            }
+            return total2;
+        }
+
+        public static int Divide(int divNumber1, params int[] divNumber2)
+        {
+
+            foreach (int x in divNumber2)
+            {
+                divNumber1 /= x;
+            }
+            return divNumber1;
+        }
+        public static int Minus(params int[] minNumbers)
+        {
+            var total4 = minNumbers[0];
+
+            for (int i = 1; i < minNumbers.Length; i++)
+            {
+                total4 -= minNumbers[i];
+            }
+            return total4;
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double area;
+
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            int awnser = Methods.Sum(20, 12);
+            
+            Console.WriteLine($" 20 + 12 = {awnser}" );
+            
+            double awnser2 = Methods.Multiply(50,80);
+            
+            Console.WriteLine($"50 x 80 = {awnser2}");
+            
+            int awnser3 = Methods.Minus(89, 53);
+
+            Console.WriteLine($"89 - 53 = {awnser3}");
+            
+            int a = 17;
+            
+            int b = 4;
+
+            double quotient = Methods.Divide(a, b);
+
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} with a remainder of {remainder}");
+
+            Console.WriteLine("What is the Radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+            double area = Methods.AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+
+
+
+        }
+    }
+}
